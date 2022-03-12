19e4a139a2d65bc168dfd2ca5b95a06c2116aa6a
Adrienne Hembrick <43283730+hembrickad@users.noreply.github.com>
2022-03-12 10:00:15 -0500

    added config

---
+++ config.py
@@ -0,0 +1,22 @@
+import configparser
+
+config = configparser.ConfigParser()
+config['DIRECTORY'] = 'ImageAnalysis/cell_smears'
+config['DEFAULT'] = 
+{'pixel_val_grey':,
+'pixel_val_color':,
+'snp':,
+'gausNoise':,
+'speckle':,
+'histo_one':,
+
+
+
+
+}
+config['OTHER_METHODS'] = {
+
+
+
+
+}
