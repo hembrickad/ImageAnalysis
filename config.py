c719e5770c757106139b4fa82e61032b63dbdb48
Adrienne Hembrick <43283730+hembrickad@users.noreply.github.com>
2022-03-13 13:30:34 -0400

    .

--- config.py
+++ config.py
@@ -1,22 +1,17 @@
 import configparser
 
 config = configparser.ConfigParser()
-config['DIRECTORY'] = 'ImageAnalysis/cell_smears'
-config['DEFAULT'] = 
-{'pixel_val_grey':,
-'pixel_val_color':,
-'snp':,
-'gausNoise':,
-'speckle':,
-'histo_one':,
-
-
-
-
+config['DEFAULT'] = {'directory': 'ImageAnalysis/cell_smears',
+'pixel_val_grey':{'run': 'True', 'channel': 'k'},
+'histo_one':{'run': 'True'},
+'histo_avg':{'run':'True'}
 }
-config['OTHER_METHODS'] = {
-
-
-
-
+config['OTHER'] = {'pixel_val_color':{'run': 'False', 'channel': 'r'},
+'snp':{'run': 'False', 'strength': '25'},
+'gausNoise':{'run': 'False', 'strength': '25'},
+'speckle':{'run': 'False', 'strength': '25'},
+'histo_equal':{'run':'False'},
+'histo_quant':{'run':'False','strength': '2'},
+'LFilter':{'run': 'False', 'size':'2', 'scale': '2' },
+'MFilter':{'run': 'False', 'size': '2', 'scale': '2' }
 }
