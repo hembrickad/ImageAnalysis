6b3ad33712aba720f54cfcf2ed76eb164ba615ad
Adrienne Hembrick <43283730+hembrickad@users.noreply.github.com>
2022-03-14 23:31:13 -0400

    Added quantiziation

--- config.py
+++ config.py
@@ -2,16 +2,24 @@ import configparser
 
 config = configparser.ConfigParser()
 config['DEFAULT'] = {'directory': '/Users/Adhsketch/Desktop/repos/ImageAnalysis/cell_smears/',
-'pixel_val_grey':{'run': 'True', 'channel': 'k'},
-'histo_one':{'run': 'True'},
-'histo_avg':{'run':'True'}
+'pixel_val_grey': 'True',
+'histo_one':'True',
+'histo_avg':'True',
+'pixel_val_color': 'False',
+'snp':'False',
+'gausNoise':'False',
+'speckle': 'False',
+'histo_equal':'False',
+'histo_quant':'False',
+'LFilter':'False',
+'MFilter':'False'
 }
-config['OTHER'] = {'pixel_val_color':{'run': 'False', 'channel': 'r'},
-'snp':{'run': 'False', 'strength': '25'},
-'gausNoise':{'run': 'False', 'strength': '25'},
-'speckle':{'run': 'False', 'strength': '25'},
-'histo_equal':{'run':'False'},
-'histo_quant':{'run':'False','strength': '2'},
-'LFilter':{'run': 'False', 'size':'2', 'scale': '2' },
-'MFilter':{'run': 'False', 'size': '2', 'scale': '2' }
+config['SETTINGS'] = {'pixel_val_grey': 'k',
+'pixel_val_color': 'r',
+'snp': '25',
+'gausNoise':'25',
+'speckle':'25',
+'histo_quant':'2',
+'LFilter':{'2','2'},
+'MFilter':{'2','2'}
 }
